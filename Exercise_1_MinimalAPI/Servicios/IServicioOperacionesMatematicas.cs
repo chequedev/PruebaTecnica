5d253d81f15fb68f6a2eb8d4130a836128026b16
Ezequiel L <herrera94_20@live.com>
2024-10-26 22:57:06 -0600

    Se integra Servicio de Operaciones Matematicas asi como la integracion de los end points a consumir

---
+++ Exercise_1_MinimalAPI/Servicios/IServicioOperacionesMatematicas.cs
@@ -0,0 +1,35 @@
+﻿using Exercise_1_MinimalAPI.Modelos;
+
+namespace Exercise_1_MinimalAPI.Servicios
+{
+    public interface IServicioOperacionesMatematicas : IServicios
+    {
+        /// <summary>
+        /// Operacion Suma 
+        /// </summary>
+        /// <param name="valores">objeto  que contiene los valores de los cuales realizara la operecion</param>
+        /// <returns></returns>
+        decimal Suma(Valores_Operacion valores);
+        /// <summary>
+        /// Operacion Resta 
+        /// </summary>
+        /// <param name="valores">objeto  que contiene los valores de los cuales realizara la operecion</param>
+        /// <returns></returns>
+        decimal Resta(Valores_Operacion valores);
+        /// <summary>
+        /// Operacion Divicion 
+        /// </summary>
+        /// <param name="valores">objeto  que contiene los valores de los cuales realizara la operecion</param>
+        /// <returns></returns> 
+        decimal Divicion(Valores_Operacion valores);
+        /// <summary>
+        /// Operacion Multiplicacion 
+        /// </summary>
+        /// <param name="valores">objeto  que contiene los valores de los cuales realizara la operecion</param>
+        /// <returns></returns>
+        decimal Multiplicacion(Valores_Operacion valores);
+
+
+
+    }
+}
---
+++ Exercise_1_MinimalAPI/Servicios/IServicios.cs
@@ -0,0 +1,12 @@
+﻿namespace Exercise_1_MinimalAPI.Servicios
+{
+    public interface IServicios
+    {
+        /// <summary>
+        /// Metodo para mostrar en consola la informacion  del flujo de el uso de el servicio 
+        /// </summary>
+        /// <param name="mensaje"></param>
+        void LogRegistroActividad(string mensaje);
+
+    }
+}
